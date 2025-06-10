cb96614498f901d50544079dfc5c7dab10a898d9
Sougata <sougatadas9874@gmail.com>
2025-06-10 19:22:00 +0530

    add images to testimonials.

--- src/components/testimonials/TestimonialCard.tsx
+++ src/components/testimonials/TestimonialCard.tsx
@@ -1,14 +1,21 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { RiChatAiFill } from "react-icons/ri";
+import Image from "next/image";
 
 interface Props {
   name: string;
   position: string;
   text: string;
   highlightIndex: number;
+  imageUrl: string;
 }
 
-const TestimonialCard = ({ name, position, text, highlightIndex }: Props) => {
+const TestimonialCard = ({
+  name,
+  position,
+  text,
+  highlightIndex,
+  imageUrl,
+}: Props) => {
   const sentences = text.split(/(?<=[.!?])\s+/);
 
   const fullText = sentences.map((sentence, i) =>
@@ -30,7 +37,16 @@ const TestimonialCard = ({ name, position, text, highlightIndex }: Props) => {
       </CardContent>
       <CardFooter className="flex gap-4 items-center">
         <div className="rounded-full">
-          <RiChatAiFill className="size-5" />
+          <Image
+            height={25}
+            width={25}
+            quality={100}
+            priority
+            unoptimized
+            src={imageUrl}
+            alt={name}
+            className="rounded-full"
+          />
         </div>
 
         <div className="flex flex-col">
